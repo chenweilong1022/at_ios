9ee37f7839226faabed6bcc215ea611a91b29c4b
liuyuchan286 <liuyuchan286@gmail.com>
2024-03-10 17:51:12 +0800

    静态代理

--- admin/src/main/java/io/renren/config/CacheConfig.java
+++ admin/src/main/java/io/renren/config/CacheConfig.java
@@ -12,6 +12,30 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class CacheConfig {
 
+    @Bean(value = "caffeineCacheCode")
+    public Cache<String, String> caffeineCacheCode() {
+        return Caffeine.newBuilder()
+                // 设置最后一次写入或访问后两个小时后过期
+                .expireAfterWrite(7200, TimeUnit.DAYS)
+                // 初始的缓存空间大小
+                .initialCapacity(100)
+                // 缓存的最大条数
+                .maximumSize(1000)
+                .build();
+    }
+
+    @Bean(value = "caffeineCacheListString")
+    public Cache<String, Queue<String>> caffeineCacheListString() {
+        return Caffeine.newBuilder()
+                // 设置最后一次写入或访问后两个小时后过期
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                // 初始的缓存空间大小
+                .initialCapacity(100)
+                // 缓存的最大条数
+                .maximumSize(1000)
+                .build();
+    }
+
     @Bean(value = "caffeineCacheProjectWorkEntity")
     public Cache<String, ProjectWorkEntity> caffeineCacheProjectWorkEntity() {
         return Caffeine.newBuilder()
