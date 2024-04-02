30a9d272a3873f1df8874a7725da4ebb9cc5ca0e
liuyuchan286 <liuyuchan286@gmail.com>
2024-04-03 01:39:56 +0800

    代码提交

--- admin_vue/src/views/main-navbar.vue
+++ admin_vue/src/views/main-navbar.vue
@@ -2,8 +2,8 @@
   <nav class="site-navbar" :class="'site-navbar--' + navbarLayoutType">
     <div class="site-navbar__header">
       <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
-        <a class="site-navbar__brand-lg" href="javascript:;">ATIOS</a>
-        <a class="site-navbar__brand-mini" href="javascript:;">ATIOS</a>
+        <a class="site-navbar__brand-lg" href="javascript:;">LINEABC</a>
+        <a class="site-navbar__brand-mini" href="javascript:;">LINEABC</a>
       </h1>
     </div>
     <div class="site-navbar__body clearfix">
