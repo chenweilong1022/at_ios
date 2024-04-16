52d170f918ba4a023abd25eb6251de7c9e461497
liuyuchan286 <liuyuchan286@gmail.com>
2024-04-16 15:11:02 +0800

    修改controller

--- admin/src/main/resources/static/config/index.js
+++ admin/src/main/resources/static/config/index.js
@@ -9,9 +9,10 @@
   // window.SITE_CONFIG['baseUrl'] = 'http://localhost:8880';
   window.SITE_CONFIG['baseUrl'] = 'https://api.lineabc.io/'
 
+
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '2404060028';   // 版本号(年月日时分)
+  window.SITE_CONFIG['version'] = '2404161457';   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
 })();
 
